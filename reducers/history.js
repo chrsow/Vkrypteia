8a0010d6cd3a030c79eb8b35367a4ad246a8d14d
chrsow <liang_wasin@hotmail.co.th>
2018-04-17 15:34:54 +0700

    Upadte history reducers.

--- reducers/history.js
+++ reducers/history.js
@@ -1,30 +1,37 @@
-// const LOAD   = 'vkrypteia/user/LOAD';
-// const CREATE = 'vkrypteia/user/CREATE';
-// const UPDATE = 'vkrypteia/user/UPDATE';
-// const REMOVE = 'vkrypteia/user/REMOVE';
 const UPDATE_VOTE_HISTORY = 'vkrypteia/history/UPDATE_VOTE_HISTORY';
+const UPDATE_SCAN_VOTE_HISTORY = 'vkrypteia/history/UPDATE_SCAN_VOTE_HISTORY';
 
 const initialState = {
   scanHistory: [],
   voteHistory: [{
-    // avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
     contractAddress: '0x1231234235',
     question: 'Dog > Cat ?',
     result: [14, 10]
+  },{
+    contractAddress: '0x1231234235',
+    question: 'Windows > Mac OS ?',
+    result: [0, 5]
   }]
 }
 
 export default function reducer(state = initialState, action = {}) {
+  const {scanHistory, voteHistory} = state;
   switch (action.type) {
     // do reducer stuff
     case UPDATE_VOTE_HISTORY:
-      const {scanHistory, voteHistory} = state;
       return {scanHistory, voteHistory: [...voteHistory, action.payload]}
+    case UPDATE_SCAN_VOTE_HISTORY:
+      return {voteHistory, scanHistory: [...scanHistory, action.payload]}
     default: return state;
   }
 }
 
 export const updateVoteHistory = (contractAddress, question, result) => ({
+  type: UPDATE_VOTE_HISTORY,
+  payload: {contractAddress, question, result}
+})
+
+export const updateScanVoteHistory = (contractAddress, question, result) => ({
   type: UPDATE_VOTE_HISTORY,
   payload: {contractAddress, question, result}
 })
