d9d73fc96ecb6f0a6e0045619ae6bd89eb877858
chrsow <liang_wasin@hotmail.co.th>
2018-04-18 21:43:12 +0700

    Update Fonts in App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { Font } from 'expo';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import store from './utils/store';
@@ -9,7 +10,14 @@ import RootNavigation from './navigation/RootNavigation';
 // import MainTabNavigatorWithState from './navigation/MainTabNavigator';
 
 export default class App extends React.Component {
-    
+  componentDidMount() {
+    Font.loadAsync({
+      'Montserrat-Light': require('./assets/fonts/Montserrat-Light.ttf'),
+      'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+      'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+    });
+  }
+  
   render() {
     return (
       <Provider store={store}>
