b861cac78ff60d299fd8bf1a840c325dc2148d7b
chrsow <liang_wasin@hotmail.co.th>
2018-04-18 21:34:26 +0700

    Update history reducer.

--- reducers/history.js
+++ reducers/history.js
@@ -3,15 +3,16 @@ const UPDATE_SCAN_VOTE_HISTORY = 'vkrypteia/history/UPDATE_SCAN_VOTE_HISTORY';
 
 const initialState = {
   scanHistory: [],
-  voteHistory: [{
-    contractAddress: '0x1231234235',
-    question: 'Dog > Cat ?',
-    result: [14, 10]
-  },{
-    contractAddress: '0x1231234235',
-    question: 'Windows > Mac OS ?',
-    result: [0, 5]
-  }]
+  voteHistory: []
+  // [{
+  //   contractAddress: '0x1231234235',
+  //   question: 'Dog > Cat ?',
+  //   result: [14, 10]
+  // },{
+  //   contractAddress: '0x1231234235',
+  //   question: 'Windows > Mac OS ?',
+  //   result: [0, 5]
+  // }]
 }
 
 export default function reducer(state = initialState, action = {}) {
@@ -28,10 +29,10 @@ export default function reducer(state = initialState, action = {}) {
 
 export const updateVoteHistory = (contractAddress, question, result) => ({
   type: UPDATE_VOTE_HISTORY,
-  payload: {contractAddress, question, result}
+  payload: {contractAddress, question, result: [Number(result[0]), Number(result[1])]}
 })
 
 export const updateScanVoteHistory = (contractAddress, question, result) => ({
   type: UPDATE_SCAN_VOTE_HISTORY,
-  payload: {contractAddress, question, result}
+  payload: {contractAddress, question, result: [Number(result[0]), Number(result[1])]}
 })
